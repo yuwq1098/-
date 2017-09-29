65255c1b07ed33e1fe8e6f428a3ba6c688a09273
Geek.Yu <1098654043@qq.com>
2017-09-29 10:32:14 +0800

    webpack+nuxt学习

--- vue-nuxt/pro/nuxt.config.js
+++ vue-nuxt/pro/nuxt.config.js
@@ -1,17 +1,17 @@
 var webpack = require("webpack")
-
-const vueLoader = webpackConfig.module.rules.find((rule) => rule.loader === 'vue-loader')
-vueLoader.options.loaders.sass = 'vue-style-loader!css-loader!sass-loader';
+    // 提取 CSS 到单独的文件中
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     /*
      ** Headers of the page
      */
     head: {
-        title: 'pro',
+        title: '这是喻文强尝试写的nuxt',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'keywords', name: 'keywords', content: 'geekyu、Geek.Yu、my project' },
             { hid: 'description', name: 'description', content: 'Nuxt.js project' }
         ],
         link: [
@@ -51,6 +51,15 @@ module.exports = {
                 })
             }
         },
+
+        module: {
+            loaders: [
+                { test: /\.sass$/, loader: 'vue-style-loader!css-loader!sass-loader' },
+                { test: /\.jade$/, loader: 'pug-loader' },
+                { test: /\.coffee$/, loader: "coffee-loader" },
+            ]
+        },
+
         /**
          * 引入你的插件
          */
@@ -59,7 +68,9 @@ module.exports = {
                 '$': 'jquery',
                 '_': 'lodash'
                     // ...etc.
-            })
+            }),
+            // 提取 CSS 到单独的文件中
+            new ExtractTextPlugin('app.bundle.css')
         ],
 
     }
--- vue-nuxt/pro/pages/preproccess/index.vue
+++ vue-nuxt/pro/pages/preproccess/index.vue
@@ -1,13 +1,28 @@
 <template lang="pug">
-  h1.red Hello {{ name }}!
+  div.container
+    h1.red Hello {{ name }}!
+    div.oppo 我是一个div
+    section.desc-content 不可醉心于此
 </template>
 
 <script lang="coffee">
-module.exports = data: ->
-  { name: 'World' }
+
+module.exports = 
+    data: -> { name: 'World' }
+    head: {
+        title: '可以写 jade html模板、coffeeScript js预处理、sass css预处理',
+        meta: [
+            { hid: 'keywords', name: 'keywords', content: '老子是关键字' },
+            { hid: 'description', name: 'description', content: '我有错吗？' }
+        ],
+    },
 </script>
 
 <style lang="sass">
 .red
   color: red
+.oppo
+  width: 250px
+  height: 160px
+  background: #e45
 </style>
